f1647443355d4ea565d10108ee45d3d6cfbbf36f
TranMC <tranmc2005@gmail.com>
2025-05-10 09:49:19 +0700

    Deploy github

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss],
+  base: "/bicomex-web",
   css: {
     postcss: './postcss.config.js',
   },
